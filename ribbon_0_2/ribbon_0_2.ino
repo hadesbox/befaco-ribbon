8f5b6bf58a1acbf947d8deed121e5fb923467efc
Luis Gonzalez <lgonzalez@rvspain.com>
2013-04-03 21:32:56 +0200

    First changes and testing gate1, gate2 and cv1 working

--- ribbon_0_2/ribbon_0_2.ino
+++ ribbon_0_2/ribbon_0_2.ino
@@ -30,6 +30,7 @@ unsigned int total_r = 0;             // the running total
 int average_l = 0;           // la media de los valores almacenados
 int average_r = 0;           // la media de los valores almacenados
 
+int temporal =0;
 
 void setup() {
   // set the digital pin as output:
@@ -40,20 +41,25 @@ void setup() {
   sbi(ADCSRA,ADPS2) ;
   cbi(ADCSRA,ADPS1) ;
   cbi(ADCSRA,ADPS0) ;
-  pinMode(12, OUTPUT);
+  pinMode(12, OUTPUT); //gate 1
+  pinMode(13, OUTPUT); //gate 2
+
+
+  pinMode(9, OUTPUT); //cv 1
+  pinMode(10, OUTPUT); //cv 2
+
   Setup_timer2();
   for (int thisReading = 0; thisReading < numlectura; thisReading++)
   {
     lectura_l[thisReading] = 0;
     lectura_r[thisReading] = 0;
   }  
-   
+
 }
 
 void loop()
 {
-    
-  
+
   if (pin_salida_clock) //procedemos a la lectura del valor alto
   {
     current_micros= micros();
@@ -64,11 +70,14 @@ void loop()
       total_l= total_l + lectura_l[index_l]; // add the reading to the total:  
       index_l++;
       if (index_l >= numlectura) index_l = 0;
-      
+
       average_l = total_l / numlectura; // calculate the average:
+      
+      average_l = average_l / 10;
+      average_l = average_l * 10;
     }
   }
-  
+
   if (!pin_salida_clock)//procedemos a la lectura del valor bajo
   {
     current_micros= micros();
@@ -83,64 +92,86 @@ void loop()
     }
   }
 
-
- 
- 
- 
-
-
-  
-
- 
-   
   if (timer_3 > 100) {
     timer_3= 0;
-    
+
     Serial.print("average_l: ");
     Serial.print(average_l,DEC);
-    
+
     Serial.print("    average_r: ");
     Serial.print(average_r,DEC);
-    
+
     lectura_suma=average_l+average_r;
-    
-    Serial.print("    lecturasuma");
-    Serial.println(lectura_suma,DEC);
+
+    Serial.print("    lecturasuma: ");
+    Serial.print(lectura_suma,DEC);
+
+    Serial.print("  CV1 PWM: ");
+    Serial.println(temporal,DEC);
+
   }
 
- 
+
+  if(average_l == 230){
+    digitalWrite(12, LOW); 
+    analogWrite(9,0);
+  }
+  else
+  {
+    digitalWrite(12, HIGH); 
+    temporal = 300 - average_l / 2;
+    
+    temporal = temporal / 20;
+    temporal = temporal * 20;
+    analogWrite(9,temporal);
+  }
+
+
+
+
+  if(average_r < 230){
+    digitalWrite(13, LOW); 
+  }
+  else
+  {
+    digitalWrite(13, HIGH); 
+  }
+
+
 }
 
 // timer2 setup
 // prescaler a 32, CTC mode
 void Setup_timer2() {
- 
+
   OCR2A = 0xF0;
   OCR2B = 0x00;
   TCCR2A = 0x42;    //CTC mode
   TCCR2B = 0x03;   // CTC mode, prescaler /32
   TCNT2 = 0x00;
   TIMSK2 = 0x02;
-  
+
 }
 
 // interrumpimos a 1kHz
 
 ISR(TIMER2_COMPA_vect) {
-  
+
   cli();
 
   OCR2A = 0xF0;
-  
+
   pin_salida_clock=  !pin_salida_clock; // invierto conforme se invierte OC2A para saber el estado actual de la salida
 
   previous_micros=micros();
 
   timer_3++;
-    
- 
+
+
 
   sei();
-  
+
 }
+
+
 
